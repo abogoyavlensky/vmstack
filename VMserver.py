f25f113cabfd3c47d05471246e3d692f89af5d37
Evgene Oskin <burnst@mail.ru>
2012-07-13 16:33:29 +0700

    server port, config_... work uncorrectly

--- VMserver.py
+++ VMserver.py
@@ -12,8 +12,8 @@ class VMserver():
             VMserver.DB [DBserver.DBserver] is a database of virtual machines
         """
         self.status_output = (0, 'ready to work')
-        self.IN = 'echo'
-        self.DB = DBserver.DBserver()
+        self._input = 'echo'
+        self.database = DBserver.DBserver()
         self.vm_ip = ''
         self.vm_mac = ''
         
@@ -24,13 +24,13 @@ class VMserver():
         
         Return True if it is sucsesful, False otherwise
         """
-        self.IN = 'VBoxManage createvm "' + name_vm + ' --register'
+        self._input = 'VBoxManage createvm "' + name_vm + ' --register'
 
-        if self.execute(self.IN)[0] != 0:
+        if self.execute(self._input)[0] != 0:
             print 'error in create_vm:\n' + self.status_output[1]
             return False
 
-        self.DB.add(name_vm, False, '0.0.0.0')
+        self.database.add(name_vm, False, '0.0.0.0')
         return True
             
     def start_vm(self, name_vm):
@@ -40,11 +40,17 @@ class VMserver():
         
         Return True if it is sucsesful, False otherwise
         """
-        self.IN = 'VBoxManage startvm "' + name_vm + '"'
-        if self.execute(self.IN)[0] != 0:
+        if not self.database.check_name(name_vm)[0]:
+            return False
+
+        self.config_bridge_vm(name_vm)
+
+        self._input = 'VBoxManage startvm "' + name_vm + '"'
+        if self.execute(self._input)[0] != 0:
             return 
 
-        self.DB.setStarted(name_vm, self.get_vm_ip(name_vm))
+        print(self.status_output[1])
+        self.database.set_started(name_vm, self.get_vm_ip(name_vm))
         return self.vm_ip
 
         
@@ -53,11 +59,18 @@ class VMserver():
         If detailed is True it set detail output
         """
         if detailed:
-            self.IN = 'VBoxManage list --long ' 
+            self._input = 'VBoxManage list --long ' 
         else:
-            self.IN = 'VBoxManage list '
-        self.IN = self.IN + what
-        self.execute(self.IN)
+            self._input = 'VBoxManage list '
+        self._input = self._input + what
+        self.execute(self._input)
+
+    def vm_info(self, name_vm):
+        """Execute operation which initialaize self.status_output[1]
+        like string contained info of virtual machine
+        """
+        self._input = 'VBoxManage showvminfo "' + name_vm + '"'
+        self.execute(self._input)
         
     def clone_vm(self, name_parent_vm, name_child_vm):
         """VMserver.start_vm(name_parent_vm, name_child_vm = name_parent_vm + '_cloned') -> bool
@@ -66,20 +79,21 @@ class VMserver():
         
         Return True if it is sucsesful, False otherwise
         """
-        self.IN = 'VBoxManage clonevm "' + name_parent_vm + '" --name "' + name_child_vm + '" --register'
+        self._input = 'VBoxManage clonevm "' + name_parent_vm + '" --name "' + name_child_vm + '" --register'
 
-        if self.execute(self.IN)[0] != 0:
+        if self.execute(self._input)[0] != 0:
             print 'error in clone_vm:\n' + self.status_output[1]
             return False
             
-        self.DB.add(name_child_vm, False, '0.0.0.0')
+        self.database.add(name_child_vm, False, '0.0.0.0')
+        self.config_bridge_vm(name_child_vm)
         return True
         
     def control_vm(self, name_vm, what_to_do):
         """If type in cmd command 'VBoxManage controlvm "naem_vm" "what_to_do"' it will be the same 
         """
-        self.IN = 'VBoxManage controlvm "' + name_vm + '"' + what_to_do
-        self.execute()
+        self._input = 'VBoxManage controlvm "' + name_vm + '"' + what_to_do
+        self.execute(self._input)
         
     def stop_vm(self, name_vm, safely = False):
         """VMserver.stop_vm(name_vm, safe = False) -> bool
@@ -91,12 +105,12 @@ class VMserver():
         Return True if it is sucsesful, False otherwise
         """
         if safely:
-            self.IN = 'VBoxManage controlvm "' + name_vm + '"' + ' savestate'
+            self._input = 'VBoxManage controlvm "' + name_vm + '"' + ' acpipowerbutton'
         else:
-            self.IN = 'VBoxManage controlvm "' + name_vm + '"' + ' acpipowerbutton'
-        if self.execute(self.IN)[0] !=0:
+            self._input = 'VBoxManage controlvm "' + name_vm + '"' + ' poweroff'
+        if self.execute(self._input)[0] !=0:
             return False
-        self.DB.setStoped(name_vm)
+        self.database.set_stoped(name_vm)
         return True
         
     def delete_vm(self, name_vm):
@@ -106,23 +120,23 @@ class VMserver():
         
         Return True if it is sucsesful, False otherwise
         """
-        if self.DB.check_name(name_vm)[0]:
-            self.IN = 'VBoxManage unregistervm "' + name_vm + '" --delete'
-            if self.execute(self.IN)[0] != 0:
+        if self.database.check_name(name_vm)[0]:
+            self._input = 'VBoxManage unregistervm "' + name_vm + '" --delete'
+            if self.execute(self._input)[0] != 0:
                 return False
-            self.DB.delete(name_vm)
+            self.database.delete(name_vm)
             return True
         else:
             print('You did not create this virtual machine')
 
-    def execute(self, IN):
+    def execute(self, input_command):
         """VMserver.execute(IN = self.IN) -> turple([int],[str])
 
         Execute cmd string contain in "IN".
 
         If VMserver.execute()[0] equal 0 then operation was seccussful.
         """
-        self.status_output = commands.getstatusoutput(IN)
+        self.status_output = commands.getstatusoutput(input_command)
         return self.status_output
         
     def get_statusoutput(self):
@@ -133,8 +147,12 @@ class VMserver():
         return self.status_output
 
     def check_running_vms(self, name_vm):
-        self.IN = "VBoxManage list runningvms"
-        self.execute(self.IN)
+        """VMserver.check_running_vms(name_vm) -> [bool]
+
+        Return True if find virtual machine whit name "name_vm", False otherwise
+        """
+        self._input = "VBoxManage list runningvms"
+        self.execute(self._input)
         check_name_vm = '"' + name_vm + '"'
         if check_name_vm in self.status_output[1]:
             return True
@@ -152,8 +170,15 @@ class VMserver():
         
         Nota bene: for bridgeadapter need type ifcondig on host, "en1" exemple
         """
-        self.IN = 'VBoxManage controlvm "' + name_vm + '" --nic1 bridged' + ' --bridgeadapter1 en1' + ' --nictype 82540EM' + ' --cableconnectd1 on'
-        self.execute(self.IN)
+        self._input = 'VBoxManage modifyvm "' + name_vm + '" --bridgeadapter1 en1'
+        self.execute(self._input)
+        self._input = 'VBoxManage modifyvm "' + name_vm + '" --nic1 bridged'
+        self.execute(self._input)
+        self._input = 'VBoxManage modifyvm "' + name_vm + '" --nictype1 82540EM'
+        self.execute(self._input)
+        self._input = 'VBoxManage modifyvm "' + name_vm + '" --cableconnected1 on'
+        self.execute(self._input)
+                
 
     def config_hardware_vm(self, name_vm, memory_vm = "2048", vram_vm = "32", accelerate3d_vm = "on"):
         """name_vm is a virtual machine "name"
@@ -163,8 +188,8 @@ class VMserver():
         VRAM: vram_vm in MB
         if accelarate3d is True
         """    
-        self.IN = 'VBoxManage controlvm "' + name_vm+ '" --memory ' + memoty_vm + ' --vram ' + vram_vm + ' --accelerate3d ' + accelerate3d_vm
-        self. execute(self.IN)
+        self._input = 'VBoxManage modifyvm "' + name_vm + '" --memory ' + memory_vm + ' --vram ' + vram_vm + ' --accelerate3d ' + accelerate3d_vm
+        self. execute(self._input)
 
     def get_vm_mac(self, name_vm):
         """VMserver.get_vm_mac(name_vm) -> [str]
@@ -172,8 +197,8 @@ class VMserver():
         Return MAC addres of started virtual machine with name "name_vm" [str]
 
         """
-        self.IN = 'VBoxManage showvminfo "' + name_vm + '"'
-        self.execute(self.IN)
+        self._input = 'VBoxManage showvminfo "' + name_vm + '"'
+        self.execute(self._input)
         mac_address_vm = re.search("\sNIC (\d):           MAC: (\w+)", self.status_output[1]).group(2).lower()
         self.vm_mac = ''
         for i in range(6):
@@ -182,6 +207,7 @@ class VMserver():
             else:
                 self.vm_mac = self.vm_mac + mac_address_vm[i*2 : i*2+2] + ':'
         self.vm_mac = self.vm_mac[:-1]
+        print(self.vm_mac)
         return self.vm_mac
 
     def get_vm_ip(self, name_vm):
@@ -189,8 +215,10 @@ class VMserver():
 
         Return IP addres of started virtual machine with name "name_vm" [str]
         """
-        self.IN = "arp -a | grep " + self.get_vm_mac(name_vm)
-        self.execute(self.IN)
-        self.vm_ip = re.search('[(](\d+)[.](\d+)[.](\d+)[.](\d+)[)]',self.status_output[1])
+        self._input = "arp -a | grep " + self.get_vm_mac(name_vm)
+        self.execute(self._input)
+        self.vm_ip = re.search('[(](\d+)[.](\d+)[.](\d+)[.](\d+)[)]', self.status_output[1])
+        print(self.vm_ip)
         self.vm_ip = self.vm_ip.group(0)[1:-1]
+        print(self.vm_ip)
         return self.vm_ip
