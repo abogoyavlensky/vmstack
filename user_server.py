a1512a417e0daf3f64fae24e3cc97126950ca442
Evgene Oskin <burnst@mail.ru>
2012-07-19 15:56:05 +0700

    user's part of server service

---
+++ user_server.py
@@ -0,0 +1,30 @@
+import user_table
+import db_server
+
+class USERserver():
+    """USERdatabase-Server connetor class
+    """
+    def __init__(self):
+        self.database = db_server.DBserver('server.db', (user_table.base,))
+
+    def create_user(self, name_user, password):
+        """USERserver.create_user(name_user) -> bool
+        
+        Create a user note  with name "name_user"
+        
+        Return True if it is sucsesful, False otherwise
+        """
+        return self.database.add(user_table.USER, name_user, (password,))
+
+    def delete_user(self, name_user):
+        """USERserver.delete_user(name_user) -> bool
+
+        Delete a user with name "name_user"
+        
+        Return True if it is sucsesful, False otherwise
+        """
+        return self.database.delete(user_table.USER, name_user)
+
+    def check_user(self, name_user, password):
+        return self.database.check_user(user_table.USER, name_user, password)
+        
