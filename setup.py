dc7c7b86f9c47bb04e805a3e599c77614ba075f6
Evgene Oskin <burnst@mail.ru>
2012-07-19 15:54:55 +0700

    setup file, need to be setuped sqlalchemy and tornado

--- setup.py
+++ setup.py
@@ -2,6 +2,18 @@ from distutils.core import setup
 
 setup(name='vmstack',
       version='0.1',
-      py_modules=['VMserver', 'WEBapi', 'ChangerPref', 'DBserver', 'VMtable', ],
-      data_files=[('templates', ['*.html'])]
+      py_modules=['user_server', 'vm_server', 'web_api', 'db_server', 'vm_table', 'user_table'],
+      data_files=[('sites', [
+          'templates/info.html',
+          'templates/start.html',
+          'templates/stop.html',
+          'templates/delete.html',
+          'templates/get_ip.html',
+          'templates/login.html',
+          'templates/index.html',
+          'templates/register.html',
+          'templates/clone.html',
+          'templates/info_out.html',
+          'templates/configs.html',
+      ])]
       )
