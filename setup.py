b167dd19b2ba3ffe78e3d37d1585c788bdaa5cf4
Evgene Oskin <burnst@mail.ru>
2012-07-23 13:50:36 +0700

    setup file

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
-from distutils.core import setup
+import setuptools
 
-setup(name='vmstack',
-      version='0.1',
-      py_modules=['user_server', 'vm_server', 'web_api', 'db_server', 'vm_table', 'user_table'],
-      data_files=[('sites', [
-          'templates/info.html',
-          'templates/start.html',
-          'templates/stop.html',
-          'templates/delete.html',
-          'templates/get_ip.html',
-          'templates/login.html',
-          'templates/index.html',
-          'templates/register.html',
-          'templates/clone.html',
-          'templates/info_out.html',
-          'templates/configs.html',
-      ])]
-      )
+setuptools.setup(
+    name = 'vmstack',
+    version = '0.1',
+    packages = ["vmstack"],
+    scripts = [
+    ],
+    package_data = {'vmstack': [
+        'templates/*.html',
+    ]},
+    install_requires = [
+        'tornado',
+        'sqlalchemy'
+    ],
+    entry_points = {
+        'console_scripts': [
+            'vmstack = vmstack.web_api:main'
+            ]
+    }
+)
